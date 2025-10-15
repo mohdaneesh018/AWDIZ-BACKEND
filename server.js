bd035cc53a8788cf4b5fc5d888bc3b69529b8fa9
mohdaneesh018 <mdaneesh537@gmail.com>
2025-10-15 19:08:02 +0530

    Day-2

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Express Server");
+    console.log(process.env.JMT_SECRET, "-process.env.JMT_SECRET");
+    console.log(process.env.MONGODB_URL, "-process.env.mongodb_url");
+    res.send(`Welcome to the ${process.env.MY_NAME} to the Express Server`);
+    // res.send("Welcome to the Express Server");
 });
 
 app.get("/name", (req, res) => {
     res.send("I am Mohd Aneesh");
-}); 
+});
 
 app.listen(8000, () => {
     console.log("Server is running on http://localhost:8000");
