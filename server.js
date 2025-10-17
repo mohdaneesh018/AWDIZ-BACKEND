bd9da16146fba3e649f627792c46cb0bb6dadfa0
mohdaneesh018 <mdaneesh537@gmail.com>
2025-10-17 22:44:53 +0530

    Day-3

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
+app.use(express.json());
+
+const users = [];
 
 app.get("/", (req, res) => {
     console.log(process.env.JMT_SECRET, "-process.env.JMT_SECRET");
@@ -14,6 +17,48 @@ app.get("/", (req, res) => {
 app.get("/name", (req, res) => {
     res.send("I am Mohd Aneesh");
 });
+
+app.post("/register", (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            res.send("All fields are required");
+            return;
+        }
+
+        const existingUser = users.find((u) => u.email === email);
+        if (existingUser) {
+            res.send("User already exists");
+            return;
+        }
+
+        users.push({ name, email, password });
+        res.send("Registered Successfully");
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
+
+app.post("/login", (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            res.send("Email and Password are required");
+            return;
+        }
+
+        const existingUser = users.find((u) => u.email === email);
+        if (!existingUser || existingUser.password !== password) {
+            res.send("Invalid");
+            return;
+        }
+        res.send("Login Successfully");
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
 
 app.listen(8000, () => {
     console.log("Server is running on http://localhost:8000");
