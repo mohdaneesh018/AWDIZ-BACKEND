38a6d2dec32cadb9d425a95883ff09a57f289c32
mohdaneesh018 <mdaneesh537@gmail.com>
2025-10-31 23:31:36 +0530

    Assignment Solved Login Page

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,7 +1,45 @@
-export const Login = (req, res) => {
-    res.send("Login Route");
+import User from "../model/user.model.js";
+
+export const Login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res
+                .status(400)
+                .json({ message: "Email and password are required." });
+        }
+
+        const isUserExist = await User.findOne({ email });
+        if (!isUserExist) {
+            return res.status(404).json({ message: "User not found." });
+        }
+
+        if (isUserExist.password !== password) {
+            return res.status(401).json({ message: "Invalid password." });
+        }
+
+        res.status(201).send({ message: "User Login Successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Error in Login User" });
+    }
 };
 
-export const Register = (req, res) => {
-    res.send("Register Route");
+export const Register = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const isUserExist = await User.findOne({ email: email });
+
+        if (isUserExist) {
+            return res.status(400).send({ message: "Email already exists." });
+        }
+        const user = User({ email: email, password: password });
+
+        await user.save();
+        res.status(201).send({ message: "User Registered Successfully" });
+    } catch (error) {
+        res.status(500).send({ message: "Error in Registering user" });
+        console.log(error);
+    }
 };
